e20bc75e7d400ef7818983be36a5939a59868f40
MaksymRC1 <maksymotroshko@ukr.net>
2026-04-15 21:20:42 +0300

    fixed

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,5 @@
-function slugify(tittle) {
-  return tittle.toLowerCase().split(" ").join("-");
+function slugify(title) {
+  return title.toLowerCase().split(" ").join("-");
 }
 console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
 console.log(slugify("English for developer")); // "english-for-developer"
